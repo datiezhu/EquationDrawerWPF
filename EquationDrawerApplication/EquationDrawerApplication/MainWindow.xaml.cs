be89a4436c89b83e006ec86c319116f948d0339e
Héctor Chamorro <xamo1998@gmail.com>
2018-11-20 20:09:24 +0100

    Added MXParser and Theme

---
+++ EquationDrawerApplication/EquationDrawerApplication/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using org.mariuszgromada.math.mxparser;
+using Expression = org.mariuszgromada.math.mxparser.Expression;
+
+namespace EquationDrawerApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Argument x = new Argument("x=1");
+            Expression e1 = new Expression("(x^2+7*x)/(x+x)",x);
+            if (e1.checkSyntax())
+                label.Content = e1.calculate();
+            else
+                label.Content = "Syntax Error";
+        }
+    }
+}
