ad7ae67708cb5db12c8010c35cb114e599ad0eb6
Héctor Chamorro <xamo1998@gmail.com>
2018-11-22 10:32:55 +0100

    Created Transformation.cs

---
+++ EquationDrawerApplication/EquationDrawerApplication/Transformation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+namespace EquationDrawerApplication
+{
+    internal class Transformation
+    {
+        public double minScreenX { get; set; }
+        public double minScreenY { get; set; }
+        public double minX { get; set; }
+        public double minY { get; set; }
+        public double maxScreenX { get; set; }
+        public double maxScreenY { get; set; }
+        public double maxX { get; set; }
+        public double maxY { get; set; }
+
+        private double numPoints;
+        public Transformation(Canvas canvas)
+        {
+            minScreenX = 0;
+            minScreenY = 0;
+            maxScreenX = canvas.ActualWidth;
+            maxScreenY = canvas.ActualHeight;
+            numPoints = canvas.ActualWidth;
+        }
+
+        public void setInterval(double minX, double maxX, double minY, double maxY)
+        {
+            this.minX = minX;
+            this.maxX = maxX;
+            this.minY = minY;
+            this.maxY = maxY;
+        }
+
+        public double getX(double x)
+        {
+            return minX + x * (maxX - minX) / numPoints;
+        }
+
+        public double getScreenX(double x)
+        {
+            return (maxScreenX - minScreenX) * (x - minX) / (maxX - minX) + minScreenX;
+        }
+        public double getScreenY(double y)
+        {
+            return (minScreenY - maxScreenY) * (y - minY) / (maxY - minY) + maxScreenY;
+        }
+    }
+}
